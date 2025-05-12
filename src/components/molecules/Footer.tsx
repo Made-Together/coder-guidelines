dbc2dae3acd6670115845f1be3f46659a1a68f3c
Dom <dom@together.agency>
2025-05-12 15:31:43 +0100

    Asset fixes

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -17,7 +17,7 @@ type FooterProps = {
 
 export default function Footer({ image, alt, width, height, copyright, credit }: FooterProps) {
 	return (
-		<div className="relative w-full overflow-hidden bg-black">
+		<div className="relative w-full overflow-hidden">
 			<Image src={image} className="h-full w-full object-cover" alt={alt} width={width} height={height} />
 			<div className="absolute bottom-0 left-0 w-full pb-16">
 				<div className="mx-auto max-w-[1140px] justify-between text-[12px] font-medium uppercase tracking-wider max-md:text-center md:flex md:px-12">
